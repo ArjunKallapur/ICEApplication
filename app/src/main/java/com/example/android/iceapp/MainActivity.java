ff5324070ea8473e571a4aa8b80d1da0c6b9fd85
arjun <arjun.kallapur@gmail.com>
2018-09-16 21:46:59 -0700

    Added notification feature

--- app/src/main/java/com/example/android/iceapp/MainActivity.java
+++ app/src/main/java/com/example/android/iceapp/MainActivity.java
@@ -1,22 +1,32 @@
 package com.example.android.iceapp;
 
+import android.app.Notification;
+import android.app.NotificationManager;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.support.v4.app.NotificationCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.support.v7.widget.Toolbar;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
 
     //TODO: Alert to set lock screen notification
-    //TODO: Notification
-    //TODO: Shared Preference
+
+    // Manifest Constants
     final int REQUEST_CONTACT = 0xBABA;
     final int REQUEST_PROFILE = 0xABBA;
+    private static final int NOTIFICATION_ID = 0;
+    private final String NOTIFICATION_CHANNEL = "default";
 
+    // Member Variables
     SharedPreferences m_sharedPref;
     String m_sharedPref_file = "com.example.android.iceapp";
 
@@ -24,19 +34,44 @@ public class MainActivity extends AppCompatActivity {
 
     TextView m_name_textView, m_bloodType_textView, m_additional_textView,
             m_contactName_textView, m_contactPhone_textView, m_contactRelation_textView;
+    Toolbar m_toolbar;
+    NotificationManager m_notificationManager;
+    Switch m_switch;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m_toolbar = (Toolbar) findViewById(R.id.main_toolbar);
+        setSupportActionBar(m_toolbar);
 
-        //Initialize the various TextView elements
+        //Initialize the various elements
         m_name_textView = (TextView) findViewById(R.id.name);
         m_bloodType_textView = (TextView) findViewById(R.id.blood_type);
         m_additional_textView = (TextView) findViewById(R.id.additional_information);
         m_contactName_textView = (TextView) findViewById(R.id.contact_name);
         m_contactPhone_textView = (TextView) findViewById(R.id.contact_phone);
         m_contactRelation_textView = (TextView) findViewById(R.id.relation);
+        m_switch = (Switch) findViewById(R.id.notification_switch);
+        Log.d("Switch", "Switch has been obtained");
+        //Set the listener for the switch:
+        m_switch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton compoundButton, boolean isChecked) {
+                Log.d("switch_listener","this better appear");
+                if(isChecked){ //Switch is enabled
+                    sendNotification();
+                    Log.d("Notification", "Blark");
+
+                }
+                else{ //Switch is disabled
+                    disableNotification();
+                }
+            }
+        });
+
+        //Initialize the notification manager
+        m_notificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
         //Initialize the Shared Preference File
         m_sharedPref = getSharedPreferences(m_sharedPref_file, MODE_PRIVATE);
 
@@ -122,5 +157,32 @@ public class MainActivity extends AppCompatActivity {
             m_contactRelation_textView.setText(m_contact_relation);
             m_contactRelation_textView.setVisibility(View.VISIBLE);
         }
+    }
+
+    private void sendNotification() {
+        NotificationCompat.Builder notification_builder =
+                new NotificationCompat.Builder(this,NOTIFICATION_CHANNEL)
+                .setContentTitle(getString(R.string.notification_title))
+                .setContentText("Blark")
+                .setSmallIcon(R.drawable.ic_info)
+                .setStyle(new NotificationCompat.BigTextStyle()
+                    .bigText(getNotificationText()));
+        Notification notification = notification_builder.build();
+        m_notificationManager.notify(NOTIFICATION_ID,notification);
+        Toast.makeText(getApplicationContext(), R.string.notification_enabled_message, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    private void disableNotification() {
+        m_notificationManager.cancel(NOTIFICATION_ID);
+        Toast.makeText(getApplicationContext(), getString(R.string.notification_disabled_message), Toast.LENGTH_SHORT).show();
+    }
+
+    private String getNotificationText() {
+        return getString(R.string.name) + m_name + "\n"
+                + getString(R.string.blood_type) + m_bloodType + "\n"
+                + getString(R.string.notification_divider) + "\n"
+                + getString(R.string.contact) + m_contact_name + " (" + m_contact_relation + ")\n"
+                + getString(R.string.phone_number_prompt)+ m_contact_phone;
     }
 }
